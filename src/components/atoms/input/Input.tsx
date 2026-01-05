bac8c55da22ff895ceb87f26313489ee2d705da2
CrisArranz <c.arranz.arevao@gmai.com>
2026-01-05 17:00:14 +0100

    refactor(input): update Tailwind CSS styles to match form design spec

--- src/components/atoms/input/Input.tsx
+++ src/components/atoms/input/Input.tsx
@@ -5,27 +5,26 @@ interface InputProps {
 	label: string;
 	as?: HtmlInputElements;
 	input: string;
-	placeholder?: string;
 	type?: string;
 }
 
-export const Input = ({
-	label,
-	as = 'INPUT',
-	input,
-	placeholder,
-	type,
-}: InputProps) => {
+export const Input = ({ label, as = 'INPUT', input, type }: InputProps) => {
 	const InputElement = HtmlElementMap[as];
 	return (
-		<div>
-			<Text htmlFor={input} as="LABEL" content={label} />
+		<div className="relative *:focus:outline-none">
 			<InputElement
 				value=""
 				id={input}
 				name={input}
-				placeholder={placeholder}
 				type={type}
+				autoComplete="off"
+				className="peer block w-96 h-10 bg-transparent border border-gray-300 outline-none transition-all duration-300 ease px-[15px] focus:border-blue-500"
+			/>
+			<Text
+				htmlFor={input}
+				as="LABEL"
+				content={label}
+				className="absolute cursor-text z-2 text-xs left-2.5 top-[13px] font-bold pointer-events-none text-gray-400 bg-white px-[10px] transition-all duration-300 ease peer-focus:text-[11px] peer-focus:top-[-5px] peer-focus:text-blue-500 peer-valid:text-[11px] peer-valid:top-[-5px]"
 			/>
 		</div>
 	);
