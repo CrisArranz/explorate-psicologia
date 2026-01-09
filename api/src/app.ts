5185c539cba4191b8f8e675e646a5be6c68dcc2b
CrisArranz <c.arranz.arevao@gmai.com>
2026-01-09 14:06:50 +0100

    chore: include sessionConfig and loadEmployee methods

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 import router from './config/routes/routes.config';
+import { sessionConfig, loadEmployee } from './config/session/session.config';
 import { errorHandler } from './middlewares/errors/errors.middleware';
 
 import './config/db/db.config';
@@ -8,6 +9,10 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+
+app.use(sessionConfig);
+app.use(loadEmployee);
+
 app.use('/api', router);
 
 app.use(errorHandler);
